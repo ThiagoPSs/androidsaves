4638f38b0e20f192ed0e96aaf147cebb7408b90e
ThiagoPSs <psthigo@gmail.com>
2022-09-08 19:12:35 -0300

    teste2

--- app/src/main/java/com/example/atividadequiz/MainActivity.java
+++ app/src/main/java/com/example/atividadequiz/MainActivity.java
@@ -2,31 +2,77 @@ package com.example.atividadequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView textView;
+
+    SensorManager sensorManager;
+    Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = (TextView) findViewById(R.id.textView);
+
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this,sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-        public void jogar(View a){
-            Intent intent = new Intent(this, quiz1.class);
-            startActivity(intent);
-        }
-    public void credito(View b){
+    public void jogar(View a) {
+        Intent intent = new Intent(this, quiz1.class);
+        startActivity(intent);
+    }
+
+    public void credito(View b) {
         Intent intent = new Intent(this, configuracao.class);
         startActivity(intent);
     }
-    public void site(View j){
+
+    public void site(View j) {
         Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https://masp.org.br/"));
         startActivity(intent);
     }
+
+    public void local(View q) {
+        Intent intent = new Intent(this, mapa.class);
+        startActivity(intent);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if(event.sensor.getType() == Sensor.TYPE_LIGHT){
+          textView.setText("" + event.values[0]);
+      }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
 
--- app/src/main/java/com/example/atividadequiz/fim.java
+++ app/src/main/java/com/example/atividadequiz/fim.java
@@ -2,12 +2,27 @@ package com.example.atividadequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class fim extends AppCompatActivity {
 
+    private static final int CAPTURAR_IMAGEM = 1;
+    private Uri uri;
+
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,4 +32,6 @@ public class fim extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+
 }
